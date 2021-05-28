b835539969017fb77abe8b393be4338c68a72485
leedonghu <ldh930517@gmail.com>
2021-05-28 17:11:14 +0900

    5/28

---
+++ src/ch14/JDBC11Servlet.java
@@ -0,0 +1,135 @@
+package ch14;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch14.bean.Customer;
+import ch14.bean.Employee;
+
+/**
+ * Servlet implementation class JDBC11Servlet
+ */
+@WebServlet("/JDBC11Servlet")
+public class JDBC11Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JDBC11Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("eid");
+		Employee employee = executeJDBC(id);
+		
+		request.setAttribute("emp", employee);
+		
+		String path = "/ch14/jdbc11.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+	
+private Employee executeJDBC(String id) {
+		
+		
+		String sql = "SELECT * FROM Employees WHERE EmployeeID = "+id;
+				
+		Employee employee  = null;
+		
+		String url = "jdbc:mysql:// 3.34.132.204/test";
+		String user = "root";
+		String password = "wnddkdwjdqhcjfl1";
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//클래스 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//연결
+			con = DriverManager.getConnection(url, user, password);
+
+			//statement 생성
+			stmt = con.createStatement();
+
+			//쿼리 실행
+			rs = stmt.executeQuery(sql);
+
+			//결과 탐색
+			if(rs.next()) {
+				//int id = Integer.parseInt(rs.getString(1));
+				String empId = rs.getString(1);
+				String lastName = rs.getString(2);
+				String firstName = rs.getString(3);
+				
+				employee = new Employee();
+				employee.setempId(empId);
+				employee.setLastName(lastName);
+				employee.setFirstName(firstName);
+				
+				
+				
+				
+			
+			}
+
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			//연결닫기
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return employee;
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/ch14/bean/Employee.java
@@ -0,0 +1,26 @@
+package ch14.bean;
+
+public class Employee {
+	private String empId;
+	private String lastName;
+	private String firstName;
+	
+	public String getempId() {
+		return empId;
+	}
+	public void setempId(String empId) {
+		this.empId = empId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
